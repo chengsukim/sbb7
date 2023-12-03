7c095286bf2b0510108ab68cc00ef46450b6bca0
깃chengsukim <kcs981123@naver.com>
2023-12-03 22:01:43 +0900

    sbb7

---
+++ src/test/java/com/example/sbb7/Sbb7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbb7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
